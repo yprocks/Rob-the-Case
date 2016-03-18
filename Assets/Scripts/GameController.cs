073748cb0fe42bd3ae9ae150f9fb2f2edd4c16d5
yprocks <yppatel181093@gmail.com>
2016-03-18 00:40:48 -0400

    Added Gun and sound

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public Button restart;
+	public Text checkPointText;
+	UnityStandardAssets.Characters.FirstPerson.FirstPersonController fps;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Restart(){
+		SceneManager.LoadScene ("Main");	
+	}
+
+	public void SetLevelCheckpoint(){
+		restart.gameObject.SetActive (true);
+		checkPointText.gameObject.SetActive(true);
+		fps = GameObject.FindGameObjectWithTag("Player").GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController> ();
+		fps.enabled = false;
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController: MonoBehaviour {
+
+	public GameObject gunShot;
+	public Transform gunPoint;
+	public float fireRate = 0.5F;
+
+	private GameController gameController;
+	private GameObject fire;
+	private float nextFire; 
+
+	// Use this for initialization
+	void Start () {
+		nextFire = 0F;
+		gameController = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		if (Input.GetButtonDown ("Fire1")) {
+			if (Time.time > nextFire) {
+				fire = (GameObject)Instantiate (gunShot, gunPoint.transform.position, Quaternion.identity);
+				nextFire = fireRate + Time.time;
+			}
+		}
+		Destroy (fire, 1F);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Exit") {
+			gameController.SetLevelCheckpoint ();
+		}
+	}
+}
